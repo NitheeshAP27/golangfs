7c6b3a35ddba104e9a9254ea709ffd3067ed1d0c
NitheeshAP27 <nitheeshap27@gmail.com>
2025-02-12 16:28:05 +0530

    day 2 complete

--- day3/example/helloworld.go
+++ day3/example/helloworld.go
@@ -2,8 +2,61 @@ package main
 
 import "fmt"
 
+type student struct {
+	Name string
+	Rgno float64
+	Dept string
+
+} 
+
 func main() {	
+	var q string = "hello world"
+	fmt.Println(q)
+	st :=student{Name: "student1", Rgno: 12.2 , Dept: "cs" }
+    fmt.Println(st)
+	fmt.Println("name: ",st.Name,"\nrgno: ",st.Rgno,"\ndept: ",st.Dept)
+	
+}
+func add() {	
 	var x,y int =1,2
 	var z int =x+y
     fmt.Println(z)
+}
+
+
+func ifelsedemo() {
+	var a,b int
+	fmt.Scanln(&a,&b)
+	if (a>b) {
+		fmt.Println("a is larger than b")
+	} else if (a==b) {
+		fmt.Println("a is equal to b")
+	}else {
+		fmt.Println("a is smaller than b")
+	}
+}
+
+
+func forthree() {
+	s :=0
+	for i :=0 ; i<5 ; i++ {
+		s=s+i
+	}
+	fmt.Println(s)
+}
+
+
+func forcond() {
+	n := 1
+	for n < 5 {
+		n *= 2
+	}
+	fmt.Println(n)
+}
+
+func forpythonstyle() {
+	strings := []string{"hello","world","golang","nie"}
+	for i,s := range strings {
+		fmt.Println(i,s)
+	}
 }
